93166346854ccfdb627294a9e277c693ab430ddd
reimibeta <pinsoneatra.soneatra846@gmail.com>
2021-06-02 13:19:50 +0700

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # branch
     'branch_models.apps.BranchConfig',
     # Filter
     'django_admin_listfilter_dropdown',
